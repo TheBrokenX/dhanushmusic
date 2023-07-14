963df805973045f1e38fb7884bf62a9f05eb2681
TeamInsane <115443754+TheTeamInsane@users.noreply.github.com>
2023-07-14 16:45:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/itzsambodhirajXd/DhanushXMusic")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/TheBrokenX/DhanushXMusic")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
